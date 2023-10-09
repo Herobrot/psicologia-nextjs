9233c688c7bc5b60643662f1a61f15e766bf71a3
Herobroto <herobrotelpro@gmail.com>
2023-10-09 07:10:07 -0600

    Jeshua - Avance de home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarPlus } from '@fortawesome/free-solid-svg-icons';
-
+import "./inicio.css"
 
 config.autoAddCss = false;
 
@@ -20,6 +20,18 @@ export default function Home(){
                         <FontAwesomeIcon icon={faCalendarPlus} />
                     </div>
                 </div>
+            </div>
+            <div className="ayuda">
+                <div className="h1">
+                    <h1>"Estamos aquí para ayudarte"</h1>
+                </div>
+                <p>
+                    Si piensa que tiene un problema, o no se siente bien,
+                    nosotros estamos para ayudarte. Queremos que todos puedan
+                    gozar de bienestar. Muchas personas en todo el mundo suelen
+                    tener miedo de ir al psicologo. Agende su cita, sin contratiempos
+                    y sin necesidad de salir de casa, estamos para ayudarlo.
+                </p>
             </div>
         </main>
     )
