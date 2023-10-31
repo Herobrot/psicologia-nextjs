64fd1972c08446d98a47035bebf94e5b2270e145
JDzul24 <jonidzul@gmail.com>
2023-10-31 08:46:10 -0600

    10 Componentes vista terminados dev Jonathan

---
+++ src/app/ContraOlvidada/BotonContinuar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const BotonContinuar = () => {
+  return (
+    <div className="boton-continuar">
+      <div className="text-wrapper">Continuar</div>
+    </div>
+  );
+};
---
+++ src/app/ContraOlvidada/EntradaCorreo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+export const EntradaCorreo = () => {
+  return (
+    <div className="entrada-correo">
+      <div className="overlap">
+        <div className="overlap-group-wrapper">
+          <div className="overlap-group">
+            <div className="text-wrapper">Correo</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
