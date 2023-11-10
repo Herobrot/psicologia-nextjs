e0b79a0ddbb0363b2fb47da6728e4a5deba44100
Herobroto <herobrotelpro@gmail.com>
2023-11-10 02:01:50 -0600

    #14 Integracion de API [devAlan] al proyecto devJeshua

--- src/api/app.js
+++ src/api/app.js
@@ -1,13 +1,38 @@
-import mongoose from "mongoose";
-import express from 'express';
-
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+const port = process.env.PORT;
 
-const port = process.env.PORT || 3000;
-
-const uri = "mongodb+srv://223208:Jaguares34.1@cluster0.swir3km.mongodb.net/Prueba";
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const uri = "mongodb+srv://223208:Jaguares34.1@cluster0.swir3km.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Se ha conectado exitosamente a la base de datos de Mongo");
+  } catch (err) {
+    console.error(err + "Error en la conexión con la base de datos");
+  }
+}
+
+run().catch(console.dir);
+
+app.use(express.json());
+
+const apiRouterCitas = require('./routes/citas')(client);
+app.use('/citas', apiRouterCitas);
+
+const apiRouterPacientes = require('./routes/pacientes')(client);
+app.use('/pacientes', apiRouterPacientes);
+
+
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en el puerto ${port}`);
+});
--- src/api/models/citaModel.js
+++ src/api/models/citaModel.js
@@ -13,6 +13,25 @@ const citaSchema = new mongoose.Schema({
     NotasCitas: String
 });
 
+citaSchema.statics.obtenerCitasConFechaEnEspañol = function(callback) {
+    this.aggregate([
+        {
+            $project: {
+                _id: 1,
+                FechaCita: {
+                    $dateToString: {
+                        date: '$FechaCita',
+                        format: '%d-%m-%Y %H:%M:%S',
+                        timezone: 'America/Mexico_City' 
+                    }
+                },
+                EstatusCita: 1,
+                NotasCitas: 1
+            }
+        }
+    ], callback);
+};
+
 const Cita = mongoose.model('Citas', citaSchema);
 
 module.exports = Cita;
