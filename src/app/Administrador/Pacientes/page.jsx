32618bb6c343b09353a47f5e0b86b13e9aa566d2
AlanSanchez12 <Alansanchez12@gmail.com>
2023-11-23 09:04:41 -0600

    25 ACTUALIZACION TODO AAAYUDA devalan

--- src/app/Administrador/Pacientes/page.jsx
+++ src/app/Administrador/Pacientes/page.jsx
@@ -7,6 +7,17 @@ import Pacientes from "./components/Pacientes";
 import { useEffect, useState } from "react";
 export default function AdminPacientes(){
     const [listaPacientes, setListaPacientes] = useState([]);
+    const [busqueda, setBusqueda] = useState("");
+    const handleSearchChange = (e) => {
+        setBusqueda(e.target.value);
+    };
+
+    const pacientesFiltrados = listaPacientes.filter(paciente => {
+        const nombre = paciente.nombre || ""; 
+        const apellido = paciente.apellido || ""; 
+        return nombre.toLowerCase().includes(busqueda.toLowerCase()) ||
+               apellido.toLowerCase().includes(busqueda.toLowerCase());
+    });
     useEffect(() => {
       
         fetch('https://apibuena.onrender.com/paciente')
@@ -34,7 +45,13 @@ export default function AdminPacientes(){
                     <h2>Pacientes.</h2>
                 </div>
                 <div className="buscador">
-                    <input type="text" name="Buscador" id="" />
+                    <input 
+                        type="text" 
+                        name="Buscador" 
+                        id="Buscador" 
+                        value={busqueda}
+                        onChange={handleSearchChange}
+                    />
                     <label htmlFor="Buscador">
                         <FontAwesomeIcon icon={faMagnifyingGlass} />
                     </label>
--- src/app/Administrador/Pendientes/page.jsx
+++ src/app/Administrador/Pendientes/page.jsx
@@ -4,21 +4,43 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import DatePicker, { registerLocale } from "react-datepicker"
 import { es } from "date-fns/locale";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { forwardRef } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import "./Pendientes.css";
-
+import { format } from 'date-fns';
 registerLocale('es', es);
 
 export default function AdminPendientes(){
-    //Constante que almacenara el arreglo de citas ya realizadas
+   
     const [contenidoPendientes, setContenidoPendientescontenidoPendientes] = useState([]);
     const [startDate1, setStartDate1] = useState(new Date());
     const [startDate2, setStartDate2] = useState(new Date());
     const [startDate3, setStartDate3] = useState(new Date());
     const [filtro, setFiltro] = useState("Más antiguo");
+    useEffect(() => {
+        fetch('https://apibuena.onrender.com/cita/') 
+            .then(response => response.json())
+            .then(data => {
+               console.log(data)
+                const citasFiltradas = data.filter(cita => cita.EstatusCita !== "Confirmada");
+                setContenidoPendientescontenidoPendientes(citasFiltradas);
+            })
+            .catch(error => {
+                console.error('Error al obtener las citas:', error);
+            });
+    }, []);
+    const ordenarCitas = (citas, filtroActual) => {
+        return citas.sort((a, b) => {
+            const fechaA = new Date(a.FechaCita);
+            const fechaB = new Date(b.FechaCita);
+            return filtroActual === "Más reciente" ? fechaB - fechaA : fechaA - fechaB;
+        });
+    };
+    useEffect(() => {
+        setContenidoPendientescontenidoPendientes(ordenarCitas([...contenidoPendientes], filtro));
+    }, [filtro]);
     const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
         <button className="DatePickerStyle" onClick={onClick} ref={ref}>
         {value}
@@ -43,12 +65,20 @@ export default function AdminPendientes(){
                     </select>
                 </div>
                 </div>
-            <div className="contenedorPendientes">
-                <div className={contenidoPendientes ? "contenidoPendientes" : "ocultarContenido"}>
-                    {/*Implementar aquí <Citas />*/}
-                    <p className={contenidoPendientes ? "ocultarTexto" : "defaultTexto"}>Sin proximas citas.</p>
-                </div>
+                <div className="contenedorPendientes">
+            <div className={contenidoPendientes.length > 0 ? "contenidoPendientes" : "ocultarContenido"}>
+                {contenidoPendientes.length > 0 ? (
+                    contenidoPendientes.map((cita, index) => (
+                        <div key={index}>
+                       
+                            <p>Fecha de la cita: {format(new Date(cita.FechaCita), 'dd/MM/yyyy')}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p className="defaultTexto">Sin próximas citas.</p>
+                )}
             </div>
+        </div>
         </>
     );
 }
