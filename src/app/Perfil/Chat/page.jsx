3c9f064d127565a90e8015332abd9007e70190e0
AlanSanchez12 <Alansanchez12@gmail.com>
2023-12-03 19:06:30 -0600

    51 Mensajes websocket dev alan

--- src/app/Perfil/Chat/page.jsx
+++ src/app/Perfil/Chat/page.jsx
@@ -1,25 +1,59 @@
-import "./Chat.css"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons"
-import { faPaperPlane } from "@fortawesome/free-regular-svg-icons"
-export default function Chat(){
-    return(
+"use client"
+import React, { useState, useEffect } from 'react';
+import "./Chat.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+
+export default function Chat() {
+    const [socket, setSocket] = useState(null);
+    const [messages, setMessages] = useState([]);
+    const [inputMessage, setInputMessage] = useState('');
+
+    useEffect(() => {
+        const newSocket = new WebSocket('ws://localhost:8080');
+        setSocket(newSocket);
+
+        newSocket.onmessage = (event) => {
+            setMessages(prev => [...prev, event.data]);
+        };
+
+        return () => newSocket.close();
+    }, []);
+
+    const sendMessage = () => {
+        if (socket && inputMessage) {
+            socket.send(inputMessage);
+            setInputMessage('');
+        }
+    };
+
+    return (
         <>
             <main>
                 <div className="cabeceraChat">
                     <FontAwesomeIcon icon={faChevronLeft} />
-                    <h2>Chat.</h2>
+                    <h2>Chat</h2>
                 </div>
 
                 <div className="contenedorChat">
-
+                    {messages.map((message, index) => (
+                        <div key={index}>{message}</div>
+                    ))}
                 </div>
 
                 <div className="botonEnviar">
-                    <input type="text" name="" id="" />
-                    <FontAwesomeIcon icon={faPaperPlane} />
+                    <input 
+                        type="text" 
+                        value={inputMessage}
+                        onChange={(e) => setInputMessage(e.target.value)}
+                        onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
+                    />
+                    <FontAwesomeIcon 
+                        icon={faPaperPlane} 
+                        onClick={sendMessage} 
+                    />
                 </div>
             </main>
         </>
-    )
+    );
 }
