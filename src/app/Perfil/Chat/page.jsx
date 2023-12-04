a1229a07a668d65bc9534d9fceaaf2f9f1114101
Herobroto <herobrotelpro@gmail.com>
2023-12-03 22:31:42 -0600

    #46 Avance de CSS en Chat devJeshua

--- src/app/Perfil/Chat/page.jsx
+++ src/app/Perfil/Chat/page.jsx
@@ -2,12 +2,15 @@ import "./Chat.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons"
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons"
+import Link from "next/link"
 export default function Chat(){
     return(
         <>
             <main>
                 <div className="cabeceraChat">
-                    <FontAwesomeIcon icon={faChevronLeft} />
+                    <Link href="/Perfil">
+                        <FontAwesomeIcon icon={faChevronLeft} />
+                    </Link>
                     <h2>Chat.</h2>
                 </div>
 
@@ -15,7 +18,7 @@ export default function Chat(){
 
                 </div>
 
-                <div className="botonEnviar">
+                <div className="pieChat">
                     <input type="text" name="" id="" />
                     <FontAwesomeIcon icon={faPaperPlane} />
                 </div>
