09ce78dc194f01999107c35dcebc2f9ca4b19826
Herobroto <herobrotelpro@gmail.com>
2023-12-06 13:17:50 -0600

    #8 Hotfix de API en Chats devJeshua

--- src/app/Perfil/Chat/page.jsx
+++ src/app/Perfil/Chat/page.jsx
@@ -17,7 +17,7 @@ export default function Chat() {
 
     useEffect(() => {
 
-        const newSocket = new WebSocket(process.env.NEXT_PUBLIC_APIRUL_WEB);
+        const newSocket = new WebSocket(process.env.NEXT_PUBLIC_APIRUL_WEB2);
     
         newSocket.onopen = () => {
             console.log("Conexión WebSocket abierta")
